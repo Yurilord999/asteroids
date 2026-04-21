4e51852d0b7799e98086e2f2d283f18561e64f43
Yurilord <Fritz-h.mann@protonmail.com>
2026-04-21 10:32:45 +0200

    adding group containers

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    #Group containers
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     #instantiate player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -25,8 +30,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         #limit to 60 FPS
