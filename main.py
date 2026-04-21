6324dd205ad40d44dd22dc4f140d95ceb0a901ae
Yurilord <Fritz-h.mann@protonmail.com>
2026-04-21 09:23:29 +0200

    Testing connection

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #Testing connection
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
