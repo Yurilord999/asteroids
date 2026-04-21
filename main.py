6cf1a82a3e77d682d7ba3e40f2efbb168abeb39e
Yurilord <Fritz-h.mann@protonmail.com>
2026-04-21 16:51:05 +0200

    shooting mechanics added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -22,16 +23,18 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
-    #instantiate player
+    #instantiate objects
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    asteroid_field = AsteroidField()
+    Asteroid_field = AsteroidField()
 
-    #Game loopa
+    #Game loop
     while True:
         log_state()
         for event in pygame.event.get():
