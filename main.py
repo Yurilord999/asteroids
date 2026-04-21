285b154f7a063c26f33ecb8477d184b1613497b8
Yurilord <Fritz-h.mann@protonmail.com>
2026-04-21 09:43:51 +0200

    Adding circle class

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    #Testing connection
+
+
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+    #Game loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +24,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        #limit to 60 FPS
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
